6a15d684eb3fe163face8f6c0c86dab18d951305
Ben Marwick <benmarwick@hotmail.com>
2026-07-06 14:14:46 +0800

    update

--- code/01-1_monday_lesson.R
+++ code/01-1_monday_lesson.R
@@ -8,6 +8,7 @@
 #   4. Orientation to R and RStudio, RStudio Projects
 #   5. using the panes, projects, terminal
 #   6. using the console, stopping it, escaping, tab complete
+#   7. creating simple objects, assignment operator
 
 
 # Assignment: screenshot of full screen of RStudio open, project option in the console we need to see your code that successfully created a simple object
